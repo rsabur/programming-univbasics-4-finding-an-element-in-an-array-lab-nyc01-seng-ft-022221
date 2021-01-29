0339404883e3527c2c4317b03704518fb56d004e
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-01-29 01:04:35 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   count = 0
-   if count < array.length do
-     value_to_find == count
+   while count < array.length do
+     if value_to_find == count
+       return count
    end
    count += 1
  end
